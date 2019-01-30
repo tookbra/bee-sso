3977bb76aec8aed7a11073e6d38926ac3d472a79
tookbra <tookbra@outlook.com>
2019-01-30 10:10:45 +0800

    :sparkles: remove consul client

--- bee-sso-service/bee-sso-account/src/main/java/com/tookbra/bee/sso/account/AccountApplication.java
+++ bee-sso-service/bee-sso-account/src/main/java/com/tookbra/bee/sso/account/AccountApplication.java
@@ -3,16 +3,12 @@ package com.tookbra.bee.sso.account;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author tookbra
  * @date 2019/1/24
  * @description
  */
-@ComponentScan(basePackages = { "com.tookbra.bee.patch.*"})
-@EnableDiscoveryClient
 @SpringBootApplication
 public class AccountApplication {
 
